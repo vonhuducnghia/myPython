c0fc0efef1de710b675aebf5a8c4096494047fd4
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-02-14 17:26:57 +0700

    update

---
+++ List_in_Python.py
@@ -0,0 +1,13 @@
+# List in Python [], seperate elements by , list contains all types of elements
+
+list1 = ['Nghia', 19, 2001.00]
+print(list1)
+
+list2 = [i for i in range(30)]
+print(list2)
+
+list3 = [[x, x*1, x*3] for x in range(1,4)]
+print(list3)
+
+list4 = list('1')   # list(iterable; strings)
+print(list4)
